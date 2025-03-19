85853136836808d00bf392c7b499c87dc28dd013
AhnafIfti <ahannafifti@gmail.com>
2025-03-19 16:42:52 +0800

    [Add]: Added task list

--- tasks_app/views.py
+++ tasks_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def task_list(request):
+    task_list_dictionary = {'title': 'List | Task'}
+    return render(request, 'task_list.html', context=task_list_dictionary)
