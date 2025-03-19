ba017498c45905c8a926e6bb46ad0d5f299ec164
AhnafIfti <ahannafifti@gmail.com>
2025-03-19 17:32:26 +0800

    [Update]: Updated category list

--- tasks_app/views.py
+++ tasks_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from tasks_app.models import Category
 
 # Create your views here.
 def task_list(request):
@@ -6,5 +7,6 @@ def task_list(request):
     return render(request, 'task_list.html', context=task_list_dictionary)
 
 def category_list(request):
-    category_list_dictionary = {'title': 'List | Category'}
+    category_list = Category.objects.order_by('name')
+    category_list_dictionary = {'categories': category_list, 'title': 'List | Category'}
     return render(request, 'category_list.html', context=category_list_dictionary)
