4757dc08e7ebf1b7706433d6c457a4c8e42475cd
AhnafIfti <ahannafifti@gmail.com>
2025-03-19 17:44:13 +0800

    [Add]: Added task detail page

--- tasks_app/urls.py
+++ tasks_app/urls.py
@@ -5,5 +5,6 @@ app_name = "tasks_app"
 
 urlpatterns = [
     path('task-list/', views.task_list, name="task_list"),
+    path('<int:task_id>/', views.task_detail, name='task_detail'),
     path('category-list/', views.category_list, name="category_list"),
 ]
--- tasks_app/views.py
+++ tasks_app/views.py
@@ -7,6 +7,31 @@ def task_list(request):
     task_list_dictionary = {'tasks': task_list, 'title': 'List | Task'}
     return render(request, 'task_list.html', context=task_list_dictionary)
 
+def task_detail(request, task_id):
+    status_options = (
+        (1, "Pending"),
+        (2, "In Progress"),
+        (3, "Completed"),
+    )
+    priority_options = (
+        (1, "Low"),
+        (2, "Medium"),
+        (3, "High"),
+    )
+
+    task_details = Task.objects.get(pk=task_id)
+    
+    for optionId, optionName in priority_options:
+        if task_details.priority == optionId:
+            task_details.priority = optionName
+
+    for optionId, optionName in status_options:
+        if task_details.status == optionId:
+            task_details.status = optionName
+
+    task_detail_dictionary = {'task_info':task_details, 'priority_options': priority_options, 'status_options': status_options, 'title': 'Detail | Task'}
+    return render(request, 'task_detail.html', context=task_detail_dictionary)
+
 def category_list(request):
     category_list = Category.objects.order_by('name')
     category_list_dictionary = {'categories': category_list, 'title': 'List | Category'}
